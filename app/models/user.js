bde328b8a65aeea50e9e28b8732efb2049130088
diwasbk <diwasbk903@gmail.com>
2025-08-23 10:33:46 +0545

    User Schema

---
+++ app/models/user.js
@@ -0,0 +1,16 @@
+import mongoose, { mongo } from "mongoose"
+mongoose.connect("mongodb://127.0.0.1:27017/blogApp");
+const userSchema = mongoose.Schema({
+    usename: String,
+    name: String,
+    email: String,
+    password: String,
+    posts:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "posts"
+        }
+    ]
+})
+
+export default mongoose.model("users", userSchema)

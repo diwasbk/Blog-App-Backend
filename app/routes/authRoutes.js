42ac06d874876e6cd33ad2a9e9f8a40442a4107f
diwasbk <diwasbk903@gmail.com>
2025-11-30 00:06:50 +0545

    Auth Controller

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -1,117 +1,12 @@
 import express from "express"
-import userModel from "../models/user.js"
-import bcrypt from "bcrypt"
-import { generateToken, jwtAuthMiddleware } from "../utils/jwt.js"
+import { AuthController } from "../controllers/authController.js"
+import { jwtAuthMiddleware } from "../utils/jwt.js"
 
 const authRouter = express.Router()
+const authController = new AuthController
 
-// Register Account
-authRouter.post("/signup", async (req, res) => {
-    try {
-        // Check if a user with the provided email already exists in the database
-        const user = await userModel.findOne({ email: req.body.email });
-        if (user) {
-            res.send({ message: "Something went wrong!", success: false })
-        } else {
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(req.body.password, salt, async (err, hash) => {
-                    const createdUser = await userModel.create({
-                        username: req.body.username,
-                        name: req.body.name,
-                        email: req.body.email,
-                        password: hash
-                    });
-                    res.send({ message: "User registered successfully!", success: true })
-                })
-            });
-        }
-    }
-    catch (err) {
-        res.send({
-            message: err.message ?? "Unknown error",
-            success: false
-        })
-    }
-});
-
-// Login Account
-authRouter.post("/login", async (req, res) => {
-    try {
-        // Check if a user with the provided email exists in the database
-        const user = await userModel.findOne({ email: req.body.email });
-        if (!user) {
-            res.send({ message: "Something went wrong!", success: false })
-        } else {
-            bcrypt.compare(req.body.password, user.password, (err, result) => {
-                if (result) {
-                    const payload = {
-                        email: user.email,
-                        name: user.name,
-                        userId: user._id
-                    }
-                    const token = generateToken(payload)
-                    res.send({
-                        message: "Logged in successfully",
-                        success: true,
-                        token: token
-                    })
-                } else {
-                    res.send({
-                        message: "Something went wrong!",
-                        success: false
-                    })
-                }
-            });
-        }
-    }
-    catch (err) {
-        res.send({
-            message: err.message ?? "Unknown error",
-            success: false
-        })
-    }
-});
-
-// Update password
-authRouter.put("/update-password", jwtAuthMiddleware, async (req, res) => {
-    try {
-        const user = await userModel.findOne({ _id: req.user.userId })
-
-        bcrypt.compare(req.body.current_password, user.password, (err, result) => {
-            if (!result) {
-                return res.send({
-                    message: "Current pasword do not match"
-                })
-            }
-            if (req.body.new_password !== req.body.confirm_new_password) {
-                return res.send({
-                    message: "New passwords do not match.",
-                    success: false
-                })
-            }
-            // Update with hashed password
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(req.body.confirm_new_password, salt, async (err, hash) => {
-                    const updatePassword = await userModel.findOneAndUpdate(
-                        { _id: req.user.userId },
-                        { $set: { password: hash } },
-                        { new: true } // return updated document
-                    )
-                    res.send({
-                        message: "Password updated successfully",
-                        result: updatePassword,
-                        success: true
-                    })
-                })
-            })
-        })
-
-    } catch (err) {
-        res.send({
-            message: err.message ?? "Unknown error",
-            success: false
-        })
-    }
-})
+authRouter.post("/signup", authController.signupUser)
+authRouter.post("/login", authController.loginUser)
+authRouter.put("/update-password", jwtAuthMiddleware, authController.updatePassword)
 
 export default authRouter

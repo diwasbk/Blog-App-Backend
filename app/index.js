741723ab80a9388b652fdf7160b7c3abeedc759e
diwasbk <diwasbk903@gmail.com>
2026-01-26 23:06:07 +0545

    Enable credentials and frontend origin for auth cookies

--- app/index.js
+++ app/index.js
@@ -3,11 +3,19 @@ import dotenv from "dotenv"
 import authRouter from "./routes/authRoute.js";
 import profileRouter from "./routes/profileRoute.js";
 import postRouter from "./routes/postRoute.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+dotenv.config();
 app.use(express.json());
-dotenv.config()
+app.use(cookieParser());
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
 
 app.use("/api/auth", authRouter)
 app.use("/api/profile", profileRouter)
